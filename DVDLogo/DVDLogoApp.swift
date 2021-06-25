3cdfaf79b1393317004f8d1fe7fb85022972f3aa
Ivan Goncharuk <goncharukivan.biz@gmail.com>
2021-06-25 15:59:22 -0400

    Initial Commit

---
+++ DVDLogo/DVDLogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVDLogoApp.swift
+//  DVDLogo
+//
+//  Created by Ivan Goncharuk on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct DVDLogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
